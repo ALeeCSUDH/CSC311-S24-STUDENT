8be5b1bcf61a0c17af30534d7071eb17fc845226
Andrew Lee <Andrew Lee@alee168@toromail.csudh.edu>
2024-02-18 11:25:18 -0800

    Assignment 1 Initial

---
+++ Assignment1/NumberArrayList.java
@@ -0,0 +1,19 @@
+//Andrew Lee (alee168@toromail.csudh.edu)
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class NumberArrayList
+{
+    
+    NumberArrayList()
+    {
+        
+    }
+	public static void main(String[] args) 
+	{
+		//NumberArrayList myList = new NumberArrayList(500);
+	}
+	
+	
+}
